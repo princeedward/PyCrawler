dd4975f8cebcbc3af3fad80df77d9b81fffb7c1a
Edward Cui <princealva@hotmail.com>
2015-04-09 01:08:28 -0400

    Edited the xpath evaluation function

--- src/Util/XpathEval.py
+++ src/Util/XpathEval.py
@@ -1,3 +1,16 @@
-import lxml
+from lxml import etree
 
 def xpathMatch(xpath, doc):
+    root = etree.fromstring(doc)
+    if type(xpath) is list:
+        result = []
+        for each_path in xpath:
+            result.append(xpathEval(each_path, root))
+        return result
+    else:
+        return xpathEval(xpath,root)
+
+# @param xpath A single xpath expression string
+# @param root Root node of the lxml element object
+def xpathEval(xpath, root):
+    pass
