fa0901abda36c6c058dbb8388870f21c5c7f7bd4
Edward Cui <princealva@hotmail.com>
2015-04-12 21:42:18 -0400

    Prepared to switch programing environment

--- src/Crawler/fetcher.py
+++ src/Crawler/fetcher.py
@@ -1,10 +1,14 @@
+from checker import UrlChecker
+
+
 # This class indicate the status of the fetcher
 class Status:
 
     # @param code Indicate the child process status
     #               200 Ready for job
-    def __init__(self, code):
+    def __init__(self, code, message=None):
         self.status = code
+        self.message = message
 
 
 # This class is the job class which including the url to crawl and other
@@ -17,5 +21,14 @@ class Job:
 
 # Fetcher function which runs in each child process
 def Fetcher(job, param, works, monitor):
+    # check the existence of the url last update (politeness handled by queue)
+
+    # document size and document type check using response header
+    # better with persistence connection
+
+    # Extract urls and create new jobs, submit dns resolve requests(async)
+
+    # do statistics and save the document and statistics (asyn)
+
     # If job has been finished succesful, indicate the main process
     monitor.put(Status(200))
