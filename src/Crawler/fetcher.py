48a25c106e415c81282b51914e20ff6d962442f2
Edward Cui <princealva@hotmail.com>
2015-04-15 02:07:55 -0400

    1.Added the regex pattern for finding urls in plain text; 2. Implemented the filter using urlparse library and implemented async dns resolver request; 3. More stuff in url extractor in fetcher

--- src/Crawler/fetcher.py
+++ src/Crawler/fetcher.py
@@ -1,7 +1,11 @@
 import lxml.html as xhtml
+import re
+import hashlib
 from .checker import UrlChecker
 from .httpclient import HttpClient, RelativeURIError
-from urlhandler import UrlHandler
+from .urlhandler import UrlHandler
+from .asyncdns import DnsBuffer
+from .urlregex import URL_REGEX
 
 
 # This class indicate the status of the fetcher
@@ -19,9 +23,11 @@ class Status:
 # information to boost the crawling
 class Job:
 
-    def __init__(self, url):
+    def __init__(self, url, params):
         self.url = url
         self.identifier = self.calculateID(self.url)
+        self.host = params["host"] if "host" in params else None
+        self.host_ip = None
 
     def setLastUpdate(self, date):
         self.last_update = date
@@ -30,9 +36,14 @@ class Job:
         self.url = url
         self.identifier = self.calculateID(self.url)
 
+    def setIP(self, ip):
+        self.host_ip = ip
+
     def calculateID(self, url):
-        # TODO: Implement checksum
-        pass
+        # TODO: Modularize this function
+        h_func = hashlib.sha1()
+        h_func.update(url)
+        return h_func.hexdigest
 
 
 def CheckContentType(type_str, allowed_types):
@@ -45,6 +56,8 @@ def CheckContentType(type_str, allowed_types):
 USER_AGENT = "penn/cis455/crawler/0.1"
 
 # Fetcher function which runs in each child process
+
+
 def Fetcher(job, param, works, monitor):
     # document size and document type check using response header
     # better with persistence connection
@@ -79,17 +92,19 @@ def Fetcher(job, param, works, monitor):
         return
     # ----------------------------------------
     # check the existence of the url last update (politeness handled by queue)
-    job.setUrl(resp_header["content-location"]) # used the real url
-    exist, records = UrlChecker(job,param,resp_header)
+    job.setUrl(resp_header["content-location"])  # used the real url
+    exist, records = UrlChecker(job, param, resp_header)
     if exist:
         monitor.put(Status(200, "Url exists and skiped"))
         return
     # Extract urls and create new jobs, submit dns resolve requests(async)
-    headers["Connection"] = "close" # TODO: Think about this, whether this is necessary
+    # TODO: Think about this, whether this is necessary
+    headers["Connection"] = "close"
     resp_header, content = http_client.request(
         job.url, method="GET", headers=headers)
-    UrlExtractor(content, param, doc_type=resp_header["content-type"],
-                 handle=UrlHandler,root_url=job.url)
+    page_urls = UrlExtractor(content, param, works,
+                             doc_type=resp_header["content-type"],
+                             handle=UrlHandler, root_url=job.url)
     # do statistics and save the document and statistics (asyn)
 
     # If job has been finished succesful, indicate the main process
@@ -102,19 +117,38 @@ def Fetcher(job, param, works, monitor):
 # @param handle Handle funtion wich will be called for each url extracted
 # @param root_url Root url for extracting relative urls
 # @return True for succeeded, False for fail
-def UrlExtractor(str_doc, param, doc_type="html", handle=None, root_url=None):
+def UrlExtractor(str_doc, param, wqueue, doc_type="html",
+                 handle=None, root_url=None):
+    url_set = set()
+    # Enable ip and port number input
+    adns_checker = DnsBuffer()
     if "html" in doc_type:
         html_tree = xhtml.fromstring(str_doc)
         if root_url:
             html_tree.make_links_absolute(root_url)
         # ignore the links in the content
         urls = html_tree.xpath('//a/@href')
-        # TODO: think about where to put the filter level url filtering
-        if handle:
-            for each_url in urls:
-                handle(each_url, param) # pack more options in "param" if need
-        return True
-    elif "xml" in doc_type:
-    elif "text/plain" in doc_type:
+    # elif "xml" in doc_type: # TODO: Think about this
+    # elif "text/plain" in doc_type:
+    elif "xml" in doc_type or "text/plain" in doc_type:
+        tmp_results = re.findall(URL_REGEX, str_doc)
+        urls = [each[0] if "http" in each[0] else "".join(["http://", each[0]])
+                for each in results if len(each[0]) > 0]
     else:
-        return False
+        return None
+    # TODO: think about where to put the filter level url filtering
+    for each_url in urls:
+        others = {}
+        if handle:
+            # pack more options in "param" if need
+            each_url, others = handle(each_url, param, opts)
+        # A quick dirty check of the duplicate url in the current page
+        if len(each_url) == 0 or each_url in url_set:
+            continue
+        new_job = Job(each_url, others)
+        wqueue.put(new_job)
+        url_set.add(each_url)
+        # An asynchronized dns resolving
+        if "domain" in others:
+            adns_checker.submit(others["domain"])
+    return url_set
--- src/Crawler/urlhandler.py
+++ src/Crawler/urlhandler.py
@@ -1,21 +1,20 @@
 # Modulerize this file for further customize
 from .fetcher import Job
 import re
+from urlparse import urlparse, urldefrag
 
 
-def UrlHandler(url, param):
+def UrlHandler(url, param, options):
     # Apply url filtering
-    if not SingleUrlFilter(url, param):
-        return
+    filtered_url, domain = SingleUrlFilter(url, param)
+    return filtered_url, {"domain": domain}
 
 
 # Url piece-wise filtering
 # @param url a string of abosulate/relative url
 # @param param A dictionary of parameters
 def SingleUrlFilter(url, param):
-    if ("#" in url) or ("http" not in url):
-        return False
-    if re.match("", url):
-        return True
-    else:
-        return False
+    result = urlparse(url)
+    domain = result.netloc
+    defraged_url = urldefrag(url)
+    return defraged_url, domain
